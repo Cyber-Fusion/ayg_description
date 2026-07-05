337357c0dee71444300486ea431351d39721cce5
Davide De Benedittis <davide.debenedittis@gmail.com>
2026-07-05 23:16:45 +0200

    Fix: manually update collision geometries to reasonable geometries

--- launch/rviz.launch.py
+++ launch/rviz.launch.py
@@ -48,7 +48,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'camera',
-            default_value='true',
+            default_value='false',
             description='Whether to include the camera in the robot model',
         ),
         robot_state_publisher_node,
